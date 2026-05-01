f020bcf77193f4900f82cf185958ea706a2525d7
Malak Khaled <Malakkhaled2008@aucegypt.edu>
2026-05-01 16:51:19 +0300

    Reapply "Remove unused generated and duplicate files"
    This reverts commit 42ca11beaa0de6b68f2a3bba53ac339cad85ded9.

--- build/CMakeFiles/untitled2_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/untitled2_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/untitled2_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
