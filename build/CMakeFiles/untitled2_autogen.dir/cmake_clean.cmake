42ca11beaa0de6b68f2a3bba53ac339cad85ded9
yomnaosama-cyber <yomnaosama@aucegypt.edu>
2026-05-01 16:42:53 +0300

    Revert "Remove unused generated and duplicate files"
    This reverts commit 11f2f41d3a93d6b6d4e91c54ca412da98bfe6d55.

---
+++ build/CMakeFiles/untitled2_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled2_autogen"
+  "untitled2_autogen/include/ui_mainwindow.h"
+  "untitled2_autogen/mocs_compilation.cpp"
+  "untitled2_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/untitled2_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
